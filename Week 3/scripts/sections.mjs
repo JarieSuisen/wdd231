0b287176cb2ad572f39b56602d15349750b15dcb
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-09-16 03:06:24 -0600

    Finished Modules lesson (with notation of errors)

---
+++ Week 3/scripts/sections.mjs
@@ -0,0 +1,46 @@
+// // THIS IS THE ORIGINAL VERSION, NO CHANGES MADE AND NONE ARE INTENDED BY THE LESSON - DOES NOT WORK, DUE TO:
+// // ReferenceError: byuiCourse is not defined
+// // WHY IS IT SUDDENLY NOT DEFINED? BECAUSE THE MODULE FORMAT BREAKS THE REFERENCE THAT RELIED ON BEING IN THE SAME DOCUMENT
+// function setSectionSelection() {
+//     const sectionSelect = document.querySelector("#sectionNumber");
+//     byuiCourse.sections.forEach((section) => {
+//         const option = document.createElement("option");
+//         option.value = section.sectionNumber;
+//         option.textContent = `${section.sectionNumber}`;
+//         sectionSelect.appendChild(option);
+//     });
+// };
+
+
+// // FIXED VERSION
+// // Add the reference of 'sections'
+function setSectionSelection(sections) {
+
+
+    const sectionSelect = document.querySelector("#sectionNumber");
+
+
+    // // replace the following so it refences 'sections' correctly:
+    // // byuiCourse.sections.forEach((section) => {
+    sections.forEach((section) => {
+
+
+        const option = document.createElement("option");
+        option.value = section.sectionNumber;
+        option.textContent = `${section.sectionNumber}`;
+        sectionSelect.appendChild(option);
+    });
+};
+
+
+
+
+// // ------------------- THIS IS THE ESPECIALLY WRONG FORMAT PROVIDED BY THE COURSE -------------------
+// export function populateSections(sections) { ... }
+// // SERIOUSLY, WHIAT IS "populateSections(sections)" SUPPOSEDLY EVER USED FOR? THIS IS BLATANTLY WRONG IN EVERY CASE AS IT IS NEVER REFERENCED NOR EVEN CONSTRUCTED.
+
+// // ------------------- THIS IS THE STILL-WRONG FORMAT THAT THE LESSON EXPECTS -------------------
+// export function setSectionSelection(sections) { ... }
+
+// // ------------------- CORRECTED VERSION -------------------
+export { setSectionSelection };
