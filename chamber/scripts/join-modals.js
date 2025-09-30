5b1e3f2f2daf0c1740ffb902fc1c27197843b792
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-09-29 22:02:40 -0600

    update

--- chamber/scripts/join-modals.js
+++ chamber/scripts/join-modals.js
@@ -7,39 +7,74 @@ const modalSilver = document.querySelector("#membership-silver");
 const modalBronze = document.querySelector("#membership-bronze");
 const modalNp = document.querySelector("#membership-np");
 
+
+
 detailSelect.addEventListener('click', () => {
-    const memberLevel = detailSelect.ATTRIBUTE_NODE('id');
-    displayMembershipDetails(memberLevel)
+
+    const memberLevel = detailSelect.parentElement.getAttribute();
+    // IS THIS CORRECT AT ALL??!!
+
+    console.log(memberLevel);
+
+    displayMembershipDetails(memberLevel);
 });
 
 function displayMembershipDetails(level) {
 
     let rank = '';
     let cost = ''; // THIS WILL BE A $Value, not a single number
+    let benefits = '';
+
+    if (level === np) {
+        rank = 'Non Profit';
+        cost = 'Free';
+        benefits = '(General Support)';
+    }
+
+    if (level === bronze) {
+        rank = 'Non Profit';
+        cost = '$20';
+        benefits = '(Some more benefits)';
+    }
+
+    if (level === silver) {
+        rank = 'Non Profit';
+        cost = '$65';
+        benefits = '(A BUNCH OF BENEFITS!)';
+    }
+
+    if (level === gold) {
+        rank = 'Gold';
+        cost = '$100';
+        benefits = '(ALL BENEFITS!)';
+    }
 
 
-    modalCard.innerHTML = `
-    <div id="modal" class="popup-membership ${}">
+    detailSelect.innerHTML = `
+    <div id="modal" class="popup-membership ${rank}">
         <button id="closeModal">❌</button>
-        <h2>Non-profit</h2>
-        <p><strong>Cost: </strong>Free</p>
-        <p><strong>Benefits: </strong> Some Benefits</p>
+        <h2>${rank}</h2>
+        <p><strong>Cost: </strong>${cost}</p>
+        <p><strong>Benefits: </strong>${benefits}</p>
     </div>
-    `
+    `;
 
     closeModal.addEventListener("click", () => {
-        modal - card.close();
+        detailSelect.close();
     });
+
+
+
 };
 
-// USE THE FOLLOWING AS THE TARGET DESIGN FOR THE GENERATED MODAL POP-UP
-<div id="modal" class="popup-membership non-profit">
-    <button id="closeModal">❌</button>
-    <h2>Non-profit</h2>
-    <p><strong>Cost: </strong>Free</p>
-    <p><strong>Benefits: </strong> Some Benefits</p>
-</div>
-// END EXAMPLE
+// // USE THE FOLLOWING AS THE TARGET DESIGN FOR THE GENERATED MODAL POP-UP
+// <div id="modal" class="popup-membership non-profit">
+//     <button id="closeModal">❌</button>
+//     <h2>Non-profit</h2>
+//     <p><strong>Cost: </strong>Free</p>
+//     <p><strong>Benefits: </strong> Some Benefits</p>
+// </div>
+// // END EXAMPLE
 
 
 
