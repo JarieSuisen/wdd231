12680f5c9cdb07e502e9ff08b8c1b37d1005ddc8
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-10-07 02:28:38 -0600

    set layout, added better video link option

--- finalproject/scripts/videolink.js
+++ finalproject/scripts/videolink.js
@@ -1,7 +1,7 @@
 const video = document.querySelector("#videolink");
 
-const url = "HEY DUMMY, YOU HAVEN'T UPDATED THIS LINK YET!";
-const words = `HELLO, THIS WILL LINK TO THE VIDEO I NEED TO UPLOAD. DO NOT FORGET, UPDATE THIS AND THE LINK.`;
+const url = "https://www.youtube.com/shorts/XRoHhQyu9SY";
+const words = `HELLO, THIS WILL LINK TO THE VIDEO I NEED TO UPLOAD. DO NOT FORGET, UPDATE THIS AND THE LINK. GO TO "videolink.js" TO UPDATE IT.`;
 
 video.setAttribute('href', url);
 video.textContent = words;
