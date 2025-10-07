a60deb695d79ac5b5c6c22d0ef53f1ca9ad3ec25
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-10-07 02:17:49 -0600

    frames about good

---
+++ finalproject/scripts/videolink.js
@@ -0,0 +1,7 @@
+const video = document.querySelector("#videolink");
+
+const url = "HEY DUMMY, YOU HAVEN'T UPDATED THIS LINK YET!";
+const words = `HELLO, THIS WILL LINK TO THE VIDEO I NEED TO UPLOAD. DO NOT FORGET, UPDATE THIS AND THE LINK.`;
+
+video.setAttribute('href', url);
+video.textContent = words;
