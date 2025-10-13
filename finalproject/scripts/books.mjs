fe34dca8878d31d6f822a4f1d70ec421a2b22973
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-10-13 03:44:56 -0600

    nearly there!

--- finalproject/scripts/books.mjs
+++ finalproject/scripts/books.mjs
@@ -1,6 +1,8 @@
 import makeTable from './books-listing.mjs';
 import { timestamp } from './timestamp.mjs';
 import { parseJson } from './books-json.mjs';
+import { storeuser } from './books-user.mjs';
+import { readuser } from './books-user.mjs';
 // import { checkout } from './books-checkout.mjs';
 
 const bookJson = './data/books.json';
@@ -19,6 +21,25 @@ makeTable(bookData);
 // checkout();
 
 timestamp();
+
+
+readuser();
+
+
+const form = document.querySelector('#reserve-form')
+form.addEventListener('submit', (e) => {
+    const userFirst = document.querySelector('input[name="first"]').value;
+    console.log(userFirst);
+
+    const userLast = document.querySelector('input[name="last"]').value;
+    console.log(userLast);
+
+    storeuser(userFirst, userLast);
+
+
+});
+
+
 
 
 // OPERATIONAL CODE HERE
